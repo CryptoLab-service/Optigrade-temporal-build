762ae442b8ecb989cda7228eb7ee9badbe71829b
TOLUWALOPE JOHN OLUWALOWO <58827223+CryptoLab-service@users.noreply.github.com>
2025-07-15 09:41:22 +0100

    Update index.ts

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -8,6 +8,6 @@ export {
   blog,
   work,
   gallery,
-} from "./content";
+} from "./content.tsx";
 
 export { display, mailchimp, routes, protectedRoutes, baseURL, fonts, style, schema, sameAs, effects, dataStyle } from "./once-ui.config";
